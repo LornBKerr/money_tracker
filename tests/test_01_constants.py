5faee000c25e04c2cea3a3b0c364d7068423130f
LornBKerr <lornburtkerr@gmail.com>
2022-11-18 11:28:43 -0500

    Added ElementType testing, revised testing the .NO_TYPE entries.

--- tests/test_01_constants.py
+++ tests/test_01_constants.py
@@ -1,17 +1,29 @@
 import os
 import sys
 
-import pytest
-
 src_path = os.path.join(os.path.realpath("."), "src")
 if src_path not in sys.path:
     sys.path.append(src_path)
 
-from constants import AccountType, BankAccountType, InvestmentAccountType
+from constants.account_types import AccountType, BankAccountType, InvestmentAccountType
+from constants.element_types import ElementType
 
 
-def test_0101_AccountType():
-    assert AccountType.NO_TYPE == 0
+def test_0101_ElementType():
+    assert ElementType.NO_TYPE == 0
+    assert ElementType.ACCOUNT
+    #    assert ElementType.SECURITY
+    #    assert ElementType.TRANSACTION
+    #    assert ElementType.CATEGORY
+    assert ElementType.NO_TYPE in ElementType.list()
+    assert ElementType.ACCOUNT in ElementType.list()
+    #    assert ElementType.SECURITY in ElementType.list()
+    #    assert ElementType.TRANSACTION in ElementType.list()
+    #    assert ElementType.CATEGORY in ElementType.list()
+
+
+def test_0102_AccountType():
+    assert AccountType.NO_TYPE
     assert AccountType.BANK
     assert AccountType.INVESTMENT
     assert AccountType.TAX
@@ -21,8 +33,8 @@ def test_0101_AccountType():
     assert AccountType.TAX in AccountType.list()
 
 
-def test_0102_BankAccountType():
-    assert BankAccountType.NO_TYPE == 0
+def test_0103_BankAccountType():
+    assert BankAccountType.NO_TYPE
     assert BankAccountType.CHECKING
     assert BankAccountType.SAVINGS
     assert BankAccountType.CD
@@ -32,8 +44,8 @@ def test_0102_BankAccountType():
     assert BankAccountType.CD in BankAccountType.list()
 
 
-def test_0103_InvestmentAccountType():
-    assert InvestmentAccountType.NO_TYPE == 0
+def test_0104_InvestmentAccountType():
+    assert InvestmentAccountType.NO_TYPE
     assert InvestmentAccountType.BROKERAGE
     assert InvestmentAccountType.SINGLE_FUND
     assert InvestmentAccountType.NO_TYPE in InvestmentAccountType.list()
