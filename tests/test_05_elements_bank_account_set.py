640042bd55ee36d6a0d92b29f6499d5e65ec3974
LornBKerr <lornburtkerr@gmail.com>
2022-11-23 12:51:48 -0500

    Correct comparison from '!=' to 'is not'. minor formating changes throughout from running black, flake8 and isort on files.'

--- tests/test_05_elements_bank_account_set.py
+++ tests/test_05_elements_bank_account_set.py
@@ -6,12 +6,6 @@ import pytest
 src_path = os.path.join(os.path.realpath("."), "src")
 if src_path not in sys.path:
     sys.path.append(src_path)
-from lbk_library import Dbal, ElementSet
-
-from constants.account_types import AccountType, BankAccountType
-from elements.bank_account import BankAccount
-from elements.bank_account_set import BankAccountSet
-
 from db_support import (
     close_database,
     create_accounts_table,
@@ -19,6 +13,11 @@ from db_support import (
     load_accounts_table,
     open_database,
 )
+from lbk_library import Dbal, ElementSet
+
+from constants.account_types import AccountType, BankAccountType
+from elements.bank_account import BankAccount
+from elements.bank_account_set import BankAccountSet
 
 
 def test_0501_constructor(create_accounts_table):
@@ -27,6 +26,7 @@ def test_0501_constructor(create_accounts_table):
     assert isinstance(account_set, BankAccountSet)
     assert isinstance(account_set, ElementSet)
     close_database(dbref)
+
 
 def test_0502_get_dbref(create_accounts_table):
     dbref = create_accounts_table
@@ -41,6 +41,7 @@ def test_0503_get_table(create_accounts_table):
     assert account_set.get_table() == "accounts"
     close_database(dbref)
 
+
 def test_0504_all_rows_empty(create_accounts_table):
     dbref = create_accounts_table
     account_set = BankAccountSet(dbref)
@@ -50,30 +51,48 @@ def test_0504_all_rows_empty(create_accounts_table):
     assert count == account_set.get_number_elements()
     close_database(dbref)
 
+
 def test_0505_all_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
     account_set = BankAccountSet(dbref)
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_type = " + str(AccountType.BANK))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_type = "
+        + str(AccountType.BANK)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     assert count == len(account_set.get_property_set())
     close_database(dbref)
+
 
 def test_0506_selected_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
-    account_set = BankAccountSet(dbref,'account_subtype', BankAccountType.CD)
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_subtype = " + str(BankAccountType.CD))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    account_set = BankAccountSet(dbref, "account_subtype", BankAccountType.CD)
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_subtype = "
+        + str(BankAccountType.CD)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     assert count == len(account_set.get_property_set())
     close_database(dbref)
+
 
 def test_0507_ordered_selected_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
-    account_set = BankAccountSet(dbref,'account_subtype', BankAccountType.CD, 'name')
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_subtype = " + str(BankAccountType.CD))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    account_set = BankAccountSet(dbref, "account_subtype", BankAccountType.CD, "name")
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_subtype = "
+        + str(BankAccountType.CD)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     selected_set = account_set.get_property_set()
     assert count == len(selected_set)
     for counter in range(0, count - 2):
@@ -81,4 +100,6 @@ def test_0507_ordered_selected_rows(create_accounts_table):
         account2 = selected_set[counter + 1]
         assert account1.get_name() < account2.get_name()
     close_database(dbref)
+
+
 ## end test_05_elements_account_set.py
--- tests/test_06_elements_investment_account_set.py
+++ tests/test_06_elements_investment_account_set.py
@@ -1,15 +1,11 @@
-import pytest
 import os
 import sys
+
+import pytest
 
 src_path = os.path.join(os.path.realpath("."), "src")
 if src_path not in sys.path:
     sys.path.append(src_path)
-
-from lbk_library import Dbal
-
-from constants.account_types import AccountType, InvestmentAccountType
-from elements.investment_account_set import InvestmentAccountSet
 
 from db_support import (
     close_database,
@@ -18,18 +14,25 @@ from db_support import (
     load_accounts_table,
     open_database,
 )
+from lbk_library import Dbal
+
+from constants.account_types import AccountType, InvestmentAccountType
+from elements.investment_account_set import InvestmentAccountSet
+
 
 def test_0601_constructor(create_accounts_table):
     dbref = create_accounts_table
     account_set = InvestmentAccountSet(dbref)
-    assert isinstance(account_set,InvestmentAccountSet)
+    assert isinstance(account_set, InvestmentAccountSet)
     close_database(dbref)
-    
+
+
 def test_0602_get_dbref(create_accounts_table):
     dbref = create_accounts_table
     account_set = InvestmentAccountSet(dbref)
     assert account_set.get_dbref() == dbref
     close_database(dbref)
+
 
 def test_0603_get_table(create_accounts_table):
     dbref = create_accounts_table
@@ -37,40 +40,63 @@ def test_0603_get_table(create_accounts_table):
     assert account_set.get_table() == "accounts"
     close_database(dbref)
 
+
 def test_0604_all_rows_empty(create_accounts_table):
     dbref = create_accounts_table
     account_set = InvestmentAccountSet(dbref)
     count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table())
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     assert count == len(account_set.get_property_set())
     assert count == account_set.get_number_elements()
     close_database(dbref)
+
 
 def test_0605_all_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
     account_set = InvestmentAccountSet(dbref)
     print(account_set.get_property_set())
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_type = " + str(AccountType.INVESTMENT))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_type = "
+        + str(AccountType.INVESTMENT)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     assert count == len(account_set.get_property_set())
     close_database(dbref)
+
 
 def test_0606_selected_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
-    account_set = InvestmentAccountSet(dbref,'account_subtype', InvestmentAccountType.SINGLE_FUND)
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_subtype = " + str(InvestmentAccountType.SINGLE_FUND))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    account_set = InvestmentAccountSet(
+        dbref, "account_subtype", InvestmentAccountType.SINGLE_FUND
+    )
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_subtype = "
+        + str(InvestmentAccountType.SINGLE_FUND)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     assert count == len(account_set.get_property_set())
     close_database(dbref)
+
 
 def test_0607_ordered_selected_rows(create_accounts_table):
     dbref = create_accounts_table
     load_accounts_table(dbref)
-    account_set = InvestmentAccountSet(dbref,'account_subtype', InvestmentAccountType.BROKERAGE, 'name')
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + account_set.get_table() + " WHERE account_subtype = " + str(InvestmentAccountType.BROKERAGE))
-    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    account_set = InvestmentAccountSet(
+        dbref, "account_subtype", InvestmentAccountType.BROKERAGE, "name"
+    )
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + account_set.get_table()
+        + " WHERE account_subtype = "
+        + str(InvestmentAccountType.BROKERAGE)
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
     selected_set = account_set.get_property_set()
     assert count == len(selected_set)
     for counter in range(0, count - 2):
@@ -78,4 +104,6 @@ def test_0607_ordered_selected_rows(create_accounts_table):
         account2 = selected_set[counter + 1]
         assert account1.get_name() < account2.get_name()
     close_database(dbref)
+
+
 # end test_06_elements_account_set.py
