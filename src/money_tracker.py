ef93815df9f183ee8c48b716bc11ce9aa2dfe4b8
LornBKerr <lornburtkerr@gmail.com>
2023-04-08 17:30:35 -0400

    Main program for the MoneyTracker program.

---
+++ src/money_tracker.py
@@ -0,0 +1,20 @@
+""" Run the 'Money Tracker' Application
+
+File:       money_tracker.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2022, 2023 Lorn B Kerr
+License:    MIT, see file License 
+"""
+
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from pages import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    # handle the system close button and exit commands
+    app.aboutToQuit.connect(main_window.exit_app_action)
+    sys.exit(app.exec())
